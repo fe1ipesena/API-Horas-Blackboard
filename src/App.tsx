d467a63aa306efdf613463a1eb5cdaecd19dc992
Felipe <felipecsena877@gmail.com>
2026-09-14 12:41:37 -0300

    feat: integra consulta de horas com API

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,61 @@
+import { useEffect, useState } from 'react';
+import { getMyHours } from './services/hours'
+
+interface HoursData {
+  ra: string
+  nome: string
+  horas_formatadas: string
+  horas_decimais: number
+  ultimo_acesso: string
+  base_atualizada_em: string
+}
+
 function App() {
-  return (
-    <div className="min-h-screen bg-black flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-white">
-        Horas Black API
-      </h1>
-    </div>
-  )
+    const [hours, setHours] = useState<HoursData | null>(null)
+
+    useEffect(() => {
+      async function loadHours() {
+        try {
+          const data = await getMyHours()
+
+          console.log('Resposta da API:', data)
+
+          setHours(data)
+        }catch (error) {
+          console.error('Erro ao consultar API:', error)
+        }
+      }
+
+      loadHours()
+    }, [])
+
+    return (
+      <div className='min-h-screen bg-black text-white flex items-center justify-center'>
+        <div className='text-center'>
+          <h1 className='text-4xl font-bold mb-6'>
+            Minhas Horas
+          </h1>
+
+          {hours ? (
+            <div>
+              <p className='text-2x1 mb-2'>
+                Olá, {hours.nome}
+              </p>
+
+              <p className='text-lg'>
+                RA: {hours.ra}
+              </p>
+
+              <p className='text-5x1 font-bold mt-6'>
+                {hours.horas_formatadas}
+              </p>
+            </div>
+          ) : (
+            <p>Consultando suas horas...</p>
+          )}
+        </div>
+      </div>
+    )
 }
 
 export default App
